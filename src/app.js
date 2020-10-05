b896da0c7a429b0167d7aaa0c805ac11e17f4215
Mahmoud Saad <43207077+Mo-Radwan@users.noreply.github.com>
2020-10-05 23:32:44 +0200

    Revert "Setup app or Heroku"
    This reverts commit 131d3eb0039a322f1a438266089199d4390562c4.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ const forecast = require('./utils/forecast')
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
-const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -129,6 +128,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
